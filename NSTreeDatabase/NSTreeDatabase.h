336aa2ef3a089e02f4e4c904fcba7a9b5115a480
Carlin <email.me@carlinyuen.com>
2013-12-21 00:16:29 -0800

    adding NSTreeDatabase files

---
+++ NSTreeDatabase/NSTreeDatabase.h
@@ -0,0 +1,13 @@
+//
+//  NSTreeDatabase.h
+//  NSTreeDatabaseExample
+//
+//  Created by . Carlin on 12/21/13.
+//  Copyright (c) 2013 Carlin Creations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTreeDatabase : NSObject
+
+@end
---
+++ NSTreeDatabase/NSTreeDatabase.m
@@ -0,0 +1,15 @@
+//
+//  NSTreeDatabase.m
+//  NSTreeDatabaseExample
+//
+//  Created by . Carlin on 12/21/13.
+//  Copyright (c) 2013 Carlin Creations. All rights reserved.
+//
+
+#import "NSTreeDatabase.h"
+
+#import "NSTree.h"
+
+@implementation NSTreeDatabase
+
+@end
