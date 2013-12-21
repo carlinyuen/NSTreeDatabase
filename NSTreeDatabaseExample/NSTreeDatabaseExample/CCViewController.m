caf03d7ed4448fa5fd29cb8d5277680777bc8797
Carlin <email.me@carlinyuen.com>
2013-12-21 00:11:06 -0800

    adding NSTree submodule

--- NSTreeDatabaseExample/NSTreeDatabaseExample/CCViewController.m
+++ NSTreeDatabaseExample/NSTreeDatabaseExample/CCViewController.m
@@ -8,7 +8,12 @@
 
 #import "CCViewController.h"
 
-@interface CCViewController ()
+    #define KEY_CELL_ID @"Cell"
+
+@interface CCViewController () <
+    UITableViewDataSource, UITableViewDelegate
+>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,7 +22,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:KEY_CELL_ID];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:KEY_CELL_ID];
+    
+    // Customize
+    
+    return cell;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Animate simple fade out
+    [tableView deselectRowAtIndexPath:indexPath animated:true];
+    
+    // Do something
+}
+
 
 @end
